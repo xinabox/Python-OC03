78634494ce40f825ae5b3d2f4a432de50c12e0b8
luqmaanb <lb@xinabox.cc>
2019-04-06 15:12:31 +0200

    Initial commit

---
+++ examples/main.py
@@ -0,0 +1,21 @@
+from OC03 import OC03
+from microbit import *
+
+OC03 = OC03.OC03()
+
+# start OC03
+OC03.init()
+
+# sleep time
+DELAY = 500
+
+# infinite loop
+while True:
+
+    # close relay
+    OC03.writePin(True)		
+    sleep(DELAY)
+    
+    # open relay
+    OC03.writePin(False)
+    sleep(DELAY)
