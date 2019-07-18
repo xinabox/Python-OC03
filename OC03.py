b5cc70ca621c4d3e0aab866c8f98113f3642feaa
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-07-18 12:43:32 +0200

    Update OC03.py
    Now uses xCore.py module

--- OC03.py
+++ OC03.py
@@ -1,4 +1,4 @@
-from microbit import i2c
+from xCore import xCore
 
 PCA9554A_I2C_ADDRESS = 0x38
 PCA9554A_REG_INPUT_PORT = 0x00
@@ -11,23 +11,24 @@ PCA9554A_CONF_INPUT = 0xFF
 
 PCA9554A_ALL_OUTPUTS_OFF = 0x00
 
-class OC03:
-    
+class xOC03:
+
     def __init__(self, addr=PCA9554A_I2C_ADDRESS):
         self.addr = addr
-     
+        self.i2c = xCore()
+
     def init(self, state=PCA9554A_ALL_OUTPUTS_OFF):
         self.writePin(state)
-        i2c.write(self.addr, bytearray([PCA9554A_REG_CONFIG, PCA9554A_CONF_OUTPUT]))
+        self.i2c.write(self.addr, bytearray([PCA9554A_REG_CONFIG, PCA9554A_CONF_OUTPUT]))
         return True
-    
+
     def writePin(self, state):
         if state is True:
-            i2c.write(self.addr,bytearray([PCA9554A_REG_OUTPUT_PORT, 0x01]))
+            self.i2c.write(self.addr,bytearray([PCA9554A_REG_OUTPUT_PORT, 0x01]))
         elif state is False:
-            i2c.write(self.addr,bytearray([PCA9554A_REG_OUTPUT_PORT, 0x00]))
-    
+            self.i2c.write(self.addr,bytearray([PCA9554A_REG_OUTPUT_PORT, 0x00]))
+
     def getStatus(self):
-        i2c.write(self.addr, bytearray([PCA9554A_REG_OUTPUT_PORT]))
-        pin_state = i2c.read(self.addr, 1)
+        self.i2c.write(self.addr, bytearray([PCA9554A_REG_OUTPUT_PORT]))
+        pin_state = self.i2c.read(self.addr, 1)
         return pin_state[0]
